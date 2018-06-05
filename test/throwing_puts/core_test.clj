fb4ff18942c05b8dabd75f7e8209e16690879c3b
vemv <vemv@users.noreply.github.com>
2018-06-05 18:02:48 +0200

    Add a test suite

--- test/throwing_puts/core_test.clj
+++ test/throwing_puts/core_test.clj
@@ -1,2 +1,48 @@
 (ns throwing-puts.core-test
-  (:require [clojure.test :refer :all]))
+  (:require
+   [clojure.core.async :as async :refer [go chan]]
+   [clojure.test :refer :all]
+   [throwing-puts.core :as throwing])
+  (:import
+   [clojure.lang ExceptionInfo]))
+
+(def timeout 100)
+
+(def message 42)
+
+(defn blocking-chan []
+  (chan))
+
+(defn nonblocking-chan []
+  (chan 1))
+
+(deftest >!!
+  (testing "Green path"
+    (is (true? (async/<!! (go
+                            (throwing/>! (nonblocking-chan) message timeout))))))
+  (testing "Red path"
+    (is (= :error (async/<!! (go
+                               (try
+                                 (throwing/>! (blocking-chan) message timeout)
+                                 (catch Exception e
+                                   :error))))))))
+
+(deftest >!!
+  (testing "Green path"
+    (is (true? (throwing/>!! (nonblocking-chan) message timeout))))
+  (testing "Red path"
+    (is (thrown? ExceptionInfo
+                 (throwing/>!! (blocking-chan) message timeout)))))
+
+(deftest dynamic-var
+  (testing "It is taken into account"
+    (let [started-at (System/currentTimeMillis)
+          timeout (- throwing/default-timeout-ms 100)
+          finished-at (binding [throwing/*default-timeout-ms* timeout]
+                        (try
+                          (throwing/>!! (blocking-chan) 42)
+                          (catch ExceptionInfo e
+                            (System/currentTimeMillis))))
+          ellapsed (- finished-at started-at)]
+      (is (>= ellapsed timeout))
+      (is (< ellapsed throwing/default-timeout-ms)))))
